68c20db6f41c772047fd085c6cf589a2cc61d7d4
hridyeshh <hridyesh2309@gmail.com>
2026-07-04 10:50:40 +0530

    leaderboard self profile

--- queries/leaderboard.sql
+++ queries/leaderboard.sql
@@ -190,10 +190,13 @@ WHERE ls.user_id = r.user_id;
 -- ============================================================================
 
 -- name: GetFriendsLeaderboard :many
+-- Includes the caller and everyone they follow, ranked together.
 SELECT ls.*
 FROM leaderboard_stats ls
-INNER JOIN follows f ON ls.user_id = f.following_id
-WHERE f.follower_id = $1
+WHERE (
+    ls.user_id = $1
+    OR ls.user_id IN (SELECT f.following_id FROM follows f WHERE f.follower_id = $1)
+  )
   AND (ls.total_xp > 0 OR ls.books_read > 0 OR ls.diary_entries > 0)
 ORDER BY ls.total_xp DESC, ls.books_read DESC
 LIMIT $2;
